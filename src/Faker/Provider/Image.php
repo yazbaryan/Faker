3f676476b94c5d143abff021aa1b19fd3bee6c86
Karen Yazbaryan <karen.yazbaryan@gmail.com>
2018-08-13 23:43:09 +0400

    Switched to picsum.photos

--- src/Faker/Provider/Image.php
+++ src/Faker/Provider/Image.php
@@ -8,8 +8,7 @@ namespace Faker\Provider;
 class Image extends Base
 {
     protected static $categories = array(
-        'abstract', 'animals', 'business', 'cats', 'city', 'food', 'nightlife',
-        'fashion', 'people', 'nature', 'sports', 'technics', 'transport'
+        'any', 'animals', 'arch', 'nature', 'people', 'tech'
     );
 
     /**
@@ -28,28 +27,10 @@ class Image extends Base
      *
      * @return string
      */
-    public static function imageUrl($width = 640, $height = 480, $category = null, $randomize = true, $word = null, $gray = false)
+    public static function imageUrl($width = 640, $height = 480, $category = 'any', $randomize = true, $word = null, $gray = false)
     {
-        $baseUrl = "https://lorempixel.com/";
+        $baseUrl = "https://picsum.photos/";
         $url = "{$width}/{$height}/";
-
-        if ($gray) {
-            $url = "gray/" . $url;
-        }
-
-        if ($category) {
-            if (!in_array($category, static::$categories)) {
-                throw new \InvalidArgumentException(sprintf('Unknown image category "%s"', $category));
-            }
-            $url .= "{$category}/";
-            if ($word) {
-                $url .= "{$word}/";
-            }
-        }
-
-        if ($randomize) {
-            $url .= '?' . static::randomNumber(5, true);
-        }
 
         return $baseUrl . $url;
     }
